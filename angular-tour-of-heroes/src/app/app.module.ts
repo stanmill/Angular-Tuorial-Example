afec7fee88c955405a65d04a37c99fc1ee61db83
stanmill <stanley.pena29@gmail.com>
2019-10-03 11:17:36 -0400

    added list of heroes

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule} from '@angular/forms';
+import { from } from 'rxjs';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
